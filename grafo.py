a0481d8e247da5e97d315519b8c6282d289bd652
edenferreria <thiago_eden@hotmail.com>
2014-05-05 18:46:59 -0300

    simplificação do código

--- grafo.py
+++ grafo.py
@@ -14,7 +14,6 @@ class Grafo:
     self.pos_pontos = defaultdict()
     self.arestas = defaultdict(list)
     self.arestas_chegando = defaultdict(list)
-    self.graus_totais = defaultdict(int)
     self.pesos = {}
     self.num_arestas = 0
 
@@ -26,50 +25,17 @@ class Grafo:
     self.pontos.add(ponto)
     self.pos_pontos[ponto] = (pos_x,pos_y)
   
-  def add_ponto_aleatorio(self,ponto):
-    #variavel de controle para sempre achar um ponto
-    cont = 0
-    de_ponto = min(self.graus_totais, key=self.graus_totais.get)
-    if self.graus_totais[de_ponto] > 2:
-      de_ponto,para_ponto = rnd.sample(self.pontos,2)
-    else:
-      para_ponto = rnd.sample(self.pontos,1)[0]
-    
-    cont = 0
-    while para_ponto == de_ponto or self.calc_distancia(de_ponto,para_ponto) > 150:
-      cont += 1
-      para_ponto = rnd.sample(self.pontos,1)[0]
-      if cont == len(self):
-        break
-
-    self.pos_pontos[ponto] = (rnd.uniform(self.pos_pontos[de_ponto][0],self.pos_pontos[para_ponto][0]),rnd.uniform(self.pos_pontos[de_ponto][1],self.pos_pontos[para_ponto][1]))
-    self.pontos.add(ponto)
-    return de_ponto,ponto,para_ponto
-
-  def add_aresta_de_para(self,de_ponto,para_ponto,peso,chance_dupla=0):
+  def add_aresta(self,de_ponto,para_ponto,peso,chance_dupla=0):
     """adiciona uma aresta saindo do ponto da esquerda para o ponto da direita
          com uma chance de adicionar uma na direção oposta se for passado o
          atributo"""
-    if de_ponto not in self.graus_totais:
-      self.graus_totais[de_ponto] = 1
-    else:
-      self.graus_totais[de_ponto] += 1
-    if para_ponto not in self.graus_totais:
-      self.graus_totais[para_ponto] = 1
-    else:
-      self.graus_totais[para_ponto] += 1
     
     self.arestas[de_ponto].append(para_ponto)
     self.arestas_chegando[para_ponto].append(de_ponto)
     self.pesos[de_ponto,para_ponto] = peso
     self.num_arestas += 1
     if rnd.randint(1,100) <= chance_dupla:
-      if de_ponto not in self.graus_totais:
-        self.graus_totais[de_ponto] = 1
-      else:
-        self.graus_totais[de_ponto] += 1
-      if para_ponto not in self.graus_totais:
-        self.graus_totais[para_ponto] = 1
+      
       self.arestas[para_ponto].append(de_ponto)
       self.arestas_chegando[de_ponto].append(para_ponto)
       self.pesos[para_ponto,de_ponto] = peso
@@ -87,31 +53,33 @@ class Grafo:
     """calcula o peso baseado na distancia e velocidade passada"""
     return self.calc_distancia(de_ponto,para_ponto) / velocidade
 
-  def criar_grafo_aleatoriamente(self,num_pontos,max_x,max_y,fra_ciclo=100,ciclo=False,chance_dupla=20):
+  def criar_grafo_aleatoriamente(self,num_pontos,max_p):
     """cria um grafo aleatoriamente, baseado no número de pontos, tamanho do ciclo gerador, e se o grafo é apenas um ciclo ou não"""
-    self.max_x = max_x
-    self.max_y = max_y
-    self.ciclo = ciclo
-    self.fra_ciclo = fra_ciclo
-    self.chance_dupla = chance_dupla
+    self.max_x = max_p
+    self.max_y = max_p
+    self.ciclo = False
+    self.fra_ciclo = num_pontos // 100
+    self.chance_dupla = 20
 
-    for i in range(fra_ciclo):
-      if i > 0:
-        self.add_ponto(i,self.get_pos_x_ponto(i-1,80),self.get_pos_y_ponto(i-1,80))    
-      else:
-        self.add_ponto(i,rnd.uniform(0,self.max_x),rnd.uniform(0,self.max_y))
-    lista_pontos = list(self.pontos)
+    for i in range(self.fra_ciclo):
+      self.add_ponto(i,rnd.uniform(0,self.max_x),rnd.uniform(0,self.max_y))
+    lista_pontos = rnd.shuffle(list(self.pontos))
     
     for i in range(len(lista_pontos)-1):
-      self.add_aresta_de_para(lista_pontos[i],lista_pontos[i+1],self.calc_peso(lista_pontos[i],lista_pontos[i+1]),self.chance_dupla)
-    self.add_aresta_de_para(lista_pontos[-1],lista_pontos[0],self.calc_peso(lista_pontos[-1],lista_pontos[0]),self.chance_dupla)
+      self.add_aresta(lista_pontos[i],lista_pontos[i+1],self.calc_peso(lista_pontos[i],lista_pontos[i+1]),self.chance_dupla)
+    self.add_aresta(lista_pontos[-1],lista_pontos[0],self.calc_peso(lista_pontos[-1],lista_pontos[0]),self.chance_dupla)
     del lista_pontos
 
     if not ciclo:
-      for ponto in range(fra_ciclo, num_pontos):
-        de_ponto, ponto, para_ponto = self.add_ponto_aleatorio(ponto)
-        self.add_aresta_de_para(de_ponto,ponto,self.calc_peso(de_ponto,ponto),self.chance_dupla)
-        self.add_aresta_de_para(ponto,para_ponto,self.calc_peso(ponto,para_ponto),self.chance_dupla)
+      for ponto in range(self.fra_ciclo, num_pontos):
+        self.add_ponto(ponto,rnd.uniform(0,self.max_x),rnd.uniform(0,self.max_y))
+        de_ponto, para_ponto = rnd.sample(self.pontos,2)
+        if de_ponto == para_ponto:
+          self.add_aresta(de_ponto,ponto,self.calc_peso(de_ponto,ponto),0)
+          self.add_aresta(ponto,para_ponto,self.calc_peso(ponto,para_ponto),0)
+        else:
+          self.add_aresta(de_ponto,ponto,self.calc_peso(de_ponto,ponto),self.chance_dupla)
+          self.add_aresta(ponto,para_ponto,self.calc_peso(ponto,para_ponto),self.chance_dupla)
 
   def armazenar_grafo(self,diretorio):
     """cria diretorio passado caso ele não exista, e armazena o grafo com a identidade"""
@@ -124,25 +92,3 @@ class Grafo:
     """gera grafo a partir de um grafo em arquivo .p"""
     with open(path,'r') as arq:
       self = pc.load(arq)
-  
-  def get_pos_x_ponto(self,ponto,dist):
-    pos_x = self.pos_pontos[ponto][0]
-    pos_x_min = pos_x - dist
-    if pos_x_min < 0:
-      pos_x_min = 0
-    pos_x_max = pos_x + dist
-    if pos_x_max > self.max_x:
-      pos_x_max = self.max_x
-
-    return rnd.uniform(pos_x_min,pos_x_max)
-  
-  def get_pos_y_ponto(self,ponto,dist):
-    pos_y = self.pos_pontos[ponto][0]
-    pos_y_min = pos_y - dist
-    if pos_y_min < 0:
-      pos_y_min = 0
-    pos_y_max = pos_y + dist
-    if pos_y_max > self.max_y:
-      pos_y_max = self.max_y
-
-    return rnd.uniform(pos_y_min,pos_y_max)
